dc361c1281ba3afdc9dcf8d2f6c9ac1b6df6f73b
IzaltinoNeto <izaltino_neto@hotmail.com>
2020-04-19 01:05:28 -0400

    implementing login and signup

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
+import api from "../../services/api";
 
 import imgB2B from '../../assets/b2bmaterial.png';
 
 export default function Logon() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+
+  function handleLogin(e) {
+    e.preventDefault();
+
+    api.post(`auth/login`, {email, password}).then((response) => {
+      console.log(response);
+      history.push('/profile');
+    }).catch( error => alert(error.response.data.message));
+    
+  }
 
   return (
     <div className="logon-container">
@@ -15,15 +29,20 @@ export default function Logon() {
       <section className="form">
         {/*  logo here */}
 
-        <form onSubmit={alert("ola")}>
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
 
-          <input placeholder="Seu e-mail"/>
-          <input placeholder="Sua senha"/>
+          <input value={email}  name="email"
+                 onChange={ (e) => setEmail(e.target.value)} 
+                 placeholder="Seu e-mail"/>
 
-          <button className="button" type="submit">Entrar</button>
+          <input value={password} 
+                  onChange={ (e) => setPassword(e.target.value)}
+                 placeholder="Sua senha"/>
 
-          <Link className="back-link" to="/#">
+          <button className="button"  type="submit">Entrar</button>
+
+          <Link className="back-link" to="/signup">
             <FiLogIn size={16} color="#FFB663"/>
             Não tenho conta
           </Link>
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './pages/Logon';
+import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Logon} />
-        <Route path="/profile" component={Profile}/>
+        <Route path="/login"  component={Logon} />
+        <Route path="/" exact component={Profile}/>
+        <Route path="/signup" component={SignUp}/>
 
       </Switch>
     </BrowserRouter>
